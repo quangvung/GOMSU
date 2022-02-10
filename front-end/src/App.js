22eeffc4643a4a71bdc7aeb03f28badf4a06ea01
Bùi Hà Nhi <buihanhi.dev@gmail.com>
2022-02-10 23:27:17 +0700

    update N

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 /* import Header from './component/layout/header'; */
 /* import Footer from './component/layout/footer'; */
 /* import About from './component/about/about';*/
-import Contact from './component/contact/contact.js';
+/* import Contact from './component/contact/contact.js';
+ */
+import Pagination from "./component/home/pagination.js";
 
 function App() {
   return (
     <div className="App">
-      <Contact/>
+      <Pagination/>
     </div>
   );
 }
---
+++ front-end/src/component/home/pagination.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+// Getting local JSON file
+import Newspapers from "./newspapers.json";
+
+function pagination() {
+  return (
+    <div>
+        <table border="2">
+            <tbody>
+                <tr>
+                    <th>Title</th>
+                    <th>Department</th>
+                </tr>
+                {Newspapers.newspapers.map((item, i) => (
+                    <tr key={i}>
+                        <td>{item.title}</td>
+                        <td>{item.submitdate}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default pagination
