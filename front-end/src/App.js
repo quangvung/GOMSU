410b2492d25bfeefaff1d9409e740e754d250da4
Nguyễn Quang Vũ <nguyenquangvu1592k@gmail.com>
2022-02-10 23:36:39 +0700

    update

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,34 +3,16 @@ import './App.css';
 /* import Header from './component/layout/header'; */
 /* import Footer from './component/layout/footer'; */
 /* import About from './component/about/about';*/
-<<<<<<< Updated upstream
-/* import Contact from './component/contact/contact.js';
- */
-import Pagination from "./component/home/pagination.js";
-=======
-<<<<<<< HEAD
 /* import Contact from './component/contact/contact.js'; */
 import Signup from './component/signup/signup.js';
 /* import PaginatedItems from './component/home/pagination.js'; */
-=======
-/* import Contact from './component/contact/contact.js';
- */
-import Pagination from "./component/home/pagination.js";
->>>>>>> 22eeffc4643a4a71bdc7aeb03f28badf4a06ea01
->>>>>>> Stashed changes
+
 
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
-      <Pagination/>
-=======
-<<<<<<< HEAD
       <Signup/>
-=======
-      <Pagination/>
->>>>>>> 22eeffc4643a4a71bdc7aeb03f28badf4a06ea01
->>>>>>> Stashed changes
+
     </div>
   );
 }
--- front-end/src/component/contact/contact.js
+++ front-end/src/component/contact/contact.js
@@ -26,7 +26,6 @@ function contact() {
       </div>
 
       <div className="div2">
-<<<<<<< HEAD
         <input type="text" className="search" />
         <SearchIcon />
 
@@ -40,23 +39,10 @@ function contact() {
           aria-hidden="false"
           tabIndex="0"
         />
-=======
-        <input type="text"/>
-        <SearchIcon/>
-        <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31327.902619918972!2d106.3419904!3d11.039539199999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x310b2f2258bb158b%3A0xdeb15205bed4b626!2sGREEN%20Department%20Store%20at%20Hai%20Chau%20Market%20Crossroads!5e0!3m2!1sen!2s!4v1644486080698!5m2!1sen!2s"
-              width="600"
-              height="450"
-              frameBorder="0"
-              style={{ border: 0 }}
-              allowFullScreen=""
-              aria-hidden="false"
-              tabIndex="0"
-            />
-<<<<<<< Updated upstream
-=======
->>>>>>> 22eeffc4643a4a71bdc7aeb03f28badf4a06ea01
->>>>>>> Stashed changes
+
+     
+       
+
       </div>
     </div>
   );
